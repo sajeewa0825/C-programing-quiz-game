6ded6e840165bf1078f3341ee630b3ce0229493a
sajeewa0825 <sajeewa0825@gmail.com>
2021-11-03 22:37:21 +0530

    create quiz add funtions

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 char selection;
+int startGame();
+int addQuiz();
+
 int main() {
     printf("\n-------------WELCOME TO THE C PROGRAMING GAME-------------\n\n");
     printf("----------------------------------------------------------\n");
     printf("Press S to start game\n");
     printf("Press V to view high score\n");
+    printf("Press A Edit quiz\n");
     printf("Press H for help\n");
     printf("Press Q to quit game\n");
     printf("----------------------------------------------------------\n");
@@ -13,7 +19,10 @@ int main() {
 
     switch (selection) {
         case 'S':
-            printf("s\n");
+            startGame();
+            break;
+        case 'a':
+            addQuiz();
             break;
         case 'V':
             printf("v \n");
@@ -22,11 +31,54 @@ int main() {
             printf("h\n");
             break;
         case 'q':
-            printf("q\n");
-            break;
+            exit(0);
         default:
             printf("invalid ");
     }
 
+    return 0;
+}
+
+int startGame() {
+    char qu[100];
+    char ans[20];
+    char stop;
+
+    FILE *file;
+    file = fopen("quiz.txt","r");
+    if (file == NULL){
+        printf("file open fail \n");
+        exit(0);
+    } else{
+        do {
+            fread(&qu ,sizeof(qu),3,file);
+            //scanf(file,"%c\n",qu);
+            printf("%s \n",qu);
+            //stop = getc(file);
+           // printf("%s ",stop);
+        } while ((int) qu == EOF);
+    }
+
+    return 0;
+}
+
+int addQuiz(){
+    char qu[100];
+    char ans[20];
+    printf("Enter Questions\n");
+    scanf("%s[^\n]",qu);
+    printf("Enter Answer\n");
+    scanf("%s[^\n]",ans);
+
+    FILE *file;
+
+    file = fopen("quiz.txt","a");
+    if(file==NULL){
+        printf("open fail\n");
+        exit(0);
+    } else{
+        fprintf(file,"%s %s\n",qu,ans);
+    }
+    fclose(file);
     return 0;
 }
